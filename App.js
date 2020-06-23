4155561181abbd982566765a37b412ee35ee7344
naoto <you@example.com>
2020-06-23 09:15:20 +0900

    sign-up, login

--- App.js
+++ App.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
-import Appbar from './src/components/Appbar';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { CardStyleInterpolators } from 'react-navigation-stack';
+import firebase from 'firebase';
+import ENV from './env.json';
 
-export default class App extends React.Component {
-  render(){
-    return (
-      <View style={styles.container}>
-        <Appbar />
-        <SignupScreen />
-        {/* <LoginScreen /> */}
-        {/* <MemoEditScreen /> */}
-        {/* <MemoDetailScreen /> */}
-        {/* <MemoListScreen /> */}
-      </View>
-    );
-  }
-}
+const config = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PRJ_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID,
+  measurementId: ENV.FIREBASE_MEASUREMENT_ID,
+};
+firebase.initializeApp(config);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFDF6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
-  },
-});
+const App = createStackNavigator({
+  Signup:     { screen: SignupScreen },
+  Login:      { screen: LoginScreen },
+  Home:       { screen: MemoListScreen },
+  MemoDetail: { screen: MemoDetailScreen },
+  MemoEdit:   { screen: MemoEditScreen },
+},
+  {
+    defaultNavigationOptions: {
+      headerTitle: 'Memot',
+      headerTitleAlign: 'center',
+      headerTintColor: '#fff',
+      headerBackTitle: null,
+      cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      headerStyle: {
+        backgroundColor: '#265366',
+      },
+      headerTitleStyle: {
+        color: '#fff',
+      },
+    },
+  });
+
+export default createAppContainer(App);
